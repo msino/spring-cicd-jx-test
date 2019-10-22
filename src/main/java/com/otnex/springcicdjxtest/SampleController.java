bfeb6e3617ea2bc6f85cc421915793c881da4410
msino <sxdone@163.com>
2019-10-22 15:03:57 +0000

    add controller

---
+++ src/main/java/com/otnex/springcicdjxtest/SampleController.java
@@ -0,0 +1,18 @@
+package com.otnex.springcicdjxtest;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Map<String, Object> showHelloWorld(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "HelloWorld");
+        return map;
+    }
+}
+
